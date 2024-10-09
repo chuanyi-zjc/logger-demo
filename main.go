53544f3d9dedb6f929d1af89cee73c5cc0d9d865
川义 <30553659+chuanyi-zjc@users.noreply.github.com>
2024-10-09 20:13:08 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "logdemo/logger"
+    "time"
+)
+
+func main() {
+    log := logger.NewLogger(logger.DEBUG)
+    go func() {
+        for {
+            log.Debug("This is a debug message.")
+            time.Sleep(100 * time.Millisecond)
+        }
+    }()
+    go func() {
+        for {
+            log.Info("This is an info message.")
+            time.Sleep(200 * time.Millisecond)
+        }
+    }()
+    go func() {
+        for {
+            log.Warning("This is a warning message.")
+            time.Sleep(300 * time.Millisecond)
+        }
+    }()
+    go func() {
+        for {
+            log.Error("This is an error message.")
+            time.Sleep(400 * time.Millisecond)
+        }
+    }()
+    time.Sleep(5 * time.Second)
+}
